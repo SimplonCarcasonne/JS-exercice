e3b906dd18457ce20a50e0b8e3e1dc8c13d9c2a9
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-05 13:54:39 +0200

    test

--- array/app.js
+++ array/app.js
@@ -1,1 +1,13 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var array = ['Pomme','Cerise','Tomate', ['Orange','Poivron']];
+
+array.push("Courgette");
+console.log (array[4]); // verif
+
+array.splice([4], 0, "citron");
+console.log (array[4]);// verif
+
+array.splice(1, 1);
+console.log(array[1]);
+
+array.splice ([2], 0, "poire");
+console.log (array[2]);
