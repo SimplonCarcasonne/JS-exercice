7c4929158f464cbf2be98735d5ccab0027e91fa1
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-05 15:10:40 +0200

    enlevé crochets

--- array/app.js
+++ array/app.js
@@ -3,11 +3,11 @@ var array = ['Pomme','Cerise','Tomate', ['Orange','Poivron']];
 array.push("Courgette");
 console.log (array[4]); // verif
 
-array.splice([4], 0, "citron");
+array[4].push("citron");
 console.log (array[4]);// verif
 
 array.splice(1, 1);
 console.log(array[1]);
 
-array.splice ([2], 0, "poire");
+array.splice (2, 0, "poire");
 console.log (array[2]);
