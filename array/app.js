0586d1bcf781982377b0fcd08947151717c244dd
mounga <christopher.fourgeaud@gmail.com>
2017-07-05 15:14:19 +0200

    all corrective

--- array/app.js
+++ array/app.js
@@ -1,3 +1,26 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
-var month = ['janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre'];
+var month = ["14eme mois","13eme mois",'janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre'];
 console.log(month);
+console.log(month[2]);
+console.log(month[5]);
+console.log(month[10]);
+
+month[7] = "août";
+console.log(month[7]);
+
+var monthLength = month.length;
+
+for(var i = 0; i < monthLength; i++){
+	console.log(month[i]);
+};
+
+array.push('courgette');
+console.log(array);
+
+array[4].push('citron');
+// console.log(array);
+
+array.splice(1, 3);
+
+array.splice(1, 1, "Poire");
+console.log(array);
