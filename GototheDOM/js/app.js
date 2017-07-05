cc1dea372c34a162f19c9d6325e23181ed89b17b
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-05 16:57:42 +0200

    exercices

---
+++ GototheDOM/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("addText").innerHTML="Hello world!";
+
+document.getElementById("remove").remove();
+
+var changeColor = document.getElementById("changeColor");
+changeColor.style.backgroundColor = "green";
+
