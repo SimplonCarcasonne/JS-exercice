923422f564be44e31e336c7b4d81fb3c19be9a0b
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-29 14:46:29 +0200

    exercice 3 et 4

--- functions.js
+++ functions.js
@@ -1,12 +1,24 @@
-1 < 2;
 
 function chaine(chaine1) {
 	return chaine1;
 }
 console.log(chaine("on essaye d'afficher un message !"));
 
-
+// fonction qui concatène 2 chaînes de caractères
 function concat(chaine1, chaine2) {
 	return chaine1 + chaine2;
 }
 console.log(concat("hello ", "Jeanne"));
+
+//exercice 4 fonction qui compare 2 nombres
+function compar (nombre1, nombre2) {
+	if (nombre1 > nombre2) {
+		return "Le premier nombre est plus grand";
+	} else if (nombre1 < nombre2){
+		return "Le deuxième nombre est le plus grand";
+	} else {
+	return "Les 2 nombres sont égaux";
+	}
+}
+
+console.log(compar(3, 4));
