17dadf943c2499940887aee0d7c0ae0b4b04965b
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-30 11:18:44 +0200

    exercice 31

--- loop/loop.js
+++ loop/loop.js
@@ -11,3 +11,11 @@ for(nb1 = 0; nb1 <= 20; nb1++) {
 	console.log(resultat);
 }
 
+//exercice 3
+var nb1 = 100;
+var nb2 = 20;
+for(nb1 = 100; nb1 > 10; nb1--) {
+	var resultat = nb1 * nb2;
+	console.log(resultat);
+}
+
