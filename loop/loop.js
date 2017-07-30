1323befca71b2c600c10bc6632c65183a13eed05
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-30 11:10:39 +0200

    exercice 21

--- loop/loop.js
+++ loop/loop.js
@@ -3,3 +3,11 @@ for(i = 0; i <= 10; i++) {
 	console.log(i);
 }
 
+//exercice 2
+var nb1 = 0;
+var nb2 = 15;
+for(nb1 = 0; nb1 <= 20; nb1++) {
+	var resultat = nb1 * nb2;
+	console.log(resultat);
+}
+
